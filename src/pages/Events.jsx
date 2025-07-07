bb0a10b4dc65e18b7e0dc1fa8b684039366b8ab8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-07 22:22:02 +0530

    Add Sidebar component and integrate it into Events page layout

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,13 +1,9 @@
+import Sidebar from "../components/Sidebar";
+
 export default function Events() {
   return (
-    <div className="container">
-      <div className="sidebar">
-
-      </div>
-      <div className="main-content">
-        <h3>Events</h3>
-        <button className="btn btn-primary">click me</button>
-      </div>
+    <div>
+      <Sidebar />
     </div>
   );
 }
