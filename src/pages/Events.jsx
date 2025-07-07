7cbbf063b8e801e417d344e0bcb50a3ffc8789ed
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-07 21:13:24 +0530

    structure for sidebar and main content for try

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,8 +1,13 @@
 export default function Events() {
   return (
-    <div>
-      <h3>Events</h3>
-      <button className="btn btn-primary">click me</button>
+    <div className="container">
+      <div className="sidebar">
+
+      </div>
+      <div className="main-content">
+        <h3>Events</h3>
+        <button className="btn btn-primary">click me</button>
+      </div>
     </div>
   );
 }
